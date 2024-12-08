8eff3f7c0f5aaff04937380ca306cf1c7cfd1236
BruderJulian <julian@familie-zech.de>
2024-12-08 15:38:48 +0100

    updated Type Definitions

--- index.d.ts
+++ index.d.ts
@@ -1,95 +1,89 @@
 type ObjectLike = object | unknown[];
+export interface HasResult {
+  depth: number;
+  Left: number;
+  object: ObjectLike;
+  key: String;
+}
+
 /**
- * static class containing the API.
+ * static class containing the API for modifying nested objects with dot/bracket notation
  * @name unPathify
  * @author BruderJulian <https://github.com/Bruderjulian>
  * @version 1.1
  */
-export as namespace unPathify
-
 export declare class unPathify {
   /**
    *  Set the property at the given path to the given value.
-   *  @param {ObjectLike} object - Object or array to set the value in.
-   *  @param {string} path - Path of the property in the object, using the path notation.
-   *  @param {unknown} value - Value to set at the path.
-   *  @throws {SyntaxError} - if the object is `undefined` or `null` or path is not a string
-   *  @throws {EvalError} - if the path could not be fully evaluated.
-   *  @static
-   *  @since 1.0
+   *  @param {ObjectLike} object - Object or array to set the value in
+   *  @param {string} path - Path of the property, using the notation
+   *  @param {unknown} value - the value to set
+   *  @throws {SyntaxError} - if the object is `undefined` or `null` or path is invalid
+   *  @throws {EvalError} - if the path could not be fully evaluated
    */
   static setProperty(object: ObjectLike, path: string, value: unknown): void;
 
   /**
    *  Get the value of the property at the given path.
    *  @param {ObjectLike} object - Object or array to get the value from.
-   *  @param {string} path - Path of the property in the object, using the path notation.
-   *  @returns {unknown} the value at the path.
-   *  @throws {SyntaxError} - if the object is `undefined` or `null` or path is not a string
+   *  @param {string} path - Path of the property, using the notation
+   *  @returns {unknown} - the value of the property
+   *  @throws {SyntaxError} - if the object is `undefined` or `null` or path is invalid
    *  @throws {EvalError} - if the path could not be fully evaluated.
-   *  @static
-   *  @since 1.0
    */
-  static getProperty(object: ObjectLike, path: string): void;
+  static getProperty(object: ObjectLike, path: string): unknown;
 
   /**
    *  Check whether the property at the given path exists.
    *  @param {ObjectLike} object - Object or array to check.
-   *  @param {string} path - Path of the property in the object, using the path notation.
-   *  @throws {SyntaxError} - if the object is `undefined` or `null` or path is not a string
+   *  @param {string} path - Path of the property, using the notation
+   *  @throws {SyntaxError} - if the object is `undefined` or `null` or path is invalid
    *  @throws {EvalError} - if the path could not be fully evaluated.
-   *  @static
-   *  @since 1.0
    */
-  static hasProperty(object: ObjectLike, path: string): boolean;
+  static hasProperty(
+    object: ObjectLike,
+    path: string,
+    detailed: boolean
+  ): boolean | HasResult;
 
   /**
    * Remove the property at the given path.
-   *  @param {ObjectLike} object - Object or array to remove the value from.
-   *  @param {string} path - Path of the property in the object, using the path notation.
-   *  @throws {SyntaxError} - if the object is `undefined` or `null` or path is not a string
+   *  @param {ObjectLike} object - Object or array to remove the property from.
+   *  @param {string} path - Path of the property, using the notation
+   *  @throws {SyntaxError} - if the object is `undefined` or `null` or path is invalid
    *  @throws {EvalError} - if the path could not be fully evaluated.
-   *  @static
-   *  @since 1.0
    */
   static removeProperty(object: ObjectLike, path: string): void;
 
   /**
    * Remove the property at the given path.
-   * @param {ObjectLike} object - Object or array to remove the value from.
-   * @param {string} path - Path of the property in the object, using the path notation.
+   * @param {ObjectLike} object - Object or array to remove the property from.
+   * @param {string} path - Path of the property, using the notation
    * @alias removeProperty
-   * @throws {SyntaxError} - if the object is `undefined` or `null` or path is not a string
+   * @throws {SyntaxError} - if the object is `undefined` or `null` or path is invalid
    * @throws {EvalError} - if the path could not be fully evaluated.
-   * @static
-   * @since 1.0
    */
   static deleteProperty(object: ObjectLike, path: string): void;
 
   /**
-   * Returns an array including every path. Non-empty plain objects and arrays are recursed and are not included themselves.
+   * Returns an array including every path. Non-empty objects or arrays are iterated and not included themselves.
    * @param {ObjectLike} object - The object to iterate through.
    * @throws {SyntaxError} - if the object is `undefined` or `null`
-   * @static
-   * @since 1.0
    */
   static keys(object: ObjectLike): string[];
 
   /**
-   * Returns an array including every path. Non-empty plain objects and arrays are recursed and are not included themselves.
+   * Returns an array including every path. Non-empty objects or arrays are iterated and not included themselves.
    * @param {ObjectLike} object - The object to iterate through.
    * @alias keys()
    * @throws {SyntaxError} - if the object is `undefined` or `null`
-   * @static
-   * @since 1.0
    */
   static getPaths(object: ObjectLike): string[];
 
   /**
-   * configures the parser and the cache
+   * configures the parser and cache
    * @param {object} [options]
    * @param {boolean} options.allowKeys - allows special keys (.constructor, .prototype, etc).
-   * @param {boolean} options.parseNumbers - parses the stringified numbers back to a number
    * @param {boolean} options.cacheSize - defines the cache size (if equal to -1, the cache won't be cleared)
    */
   static configure(options?: object): void;
@@ -97,7 +91,7 @@ export declare class unPathify {
   /**
    * Clears the entire cache
    * @static
-   * @since 1.0
+   * @since 1.1
    */
   static clearCache(): void;
 }
