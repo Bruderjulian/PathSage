e36ab3962269337a1f4ed6c43747e24e1acef3c0
BruderJulian <julian@familie-zech.de>
2024-12-14 14:46:26 +0100

    added & improved Tests for the Polyfills

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,9 +1,8 @@
 const assert = require("node:assert");
 const { describe, it } = require("node:test");
-const utils = require("../src/utils.js");
+var utils = require("../src/utils.js");
 
 describe("utils", () => {
-  /*
   it("isArray", function () {
     assert.ok(utils.isArray([]));
     assert.ok(utils.isArray(new Array()));
@@ -12,7 +11,75 @@ describe("utils", () => {
     assert.equal(utils.isArray(true), false);
     assert.equal(utils.isArray(""), false);
   });
-  */
+  it("hasOwn", function () {
+    assert.equal(utils.hasOwn({}, ""), false);
+    assert.equal(utils.hasOwn({ a: 1 }, "a"), true);
+    assert.equal(utils.hasOwn({ a: {} }, "a"), true);
+    assert.equal(utils.hasOwn({ a: [] }, "a"), true);
+    assert.equal(utils.hasOwn({}, "a"), false);
+    assert.equal(utils.hasOwn([], 0), false);
+    assert.equal(utils.hasOwn([1], 0), true);
+    assert.equal(utils.hasOwn([1], "0"), true);
+    assert.equal(utils.hasOwn([], "0"), false);
+
+    assert.equal(utils.hasOwn(Object.create(null), ""), false);
+    assert.equal(utils.hasOwn(Object.create(null), "a"), false);
+    assert.equal(utils.hasOwn({}), false);
+    assert.equal(utils.hasOwn([]), false);
+    assert.equal(utils.hasOwn(Object.create(null)), false);
+  });
+  it("entries", function () {
+    assert.deepEqual(utils.entries({ a: 1 }), [["a", 1]]);
+    assert.deepEqual(utils.entries([1]), [["0", 1]]);
+    assert.deepEqual(utils.entries({ a: 1, b: { v: 1 } }), [
+      ["a", 1],
+      ["b", { v: 1 }],
+    ]);
+    assert.deepEqual(utils.entries(Object.create(null)), []);
+    assert.deepEqual(utils.entries({}), []);
+    assert.deepEqual(utils.entries([]), []);
+  });
+  Object.entries = undefined;
+  Object.hasOwn = undefined;
+  Array.isArray = undefined;
+  utils = fresh("../src/utils.js");
+
+  it("isArray Mock", function () {
+    assert.ok(utils.isArray([]));
+    assert.ok(utils.isArray(new Array()));
+    assert.equal(utils.isArray(arguments), false);
+    assert.equal(utils.isArray({}), false);
+    assert.equal(utils.isArray(true), false);
+    assert.equal(utils.isArray(""), false);
+  });
+  it("hasOwn Mock", function () {
+    assert.equal(utils.hasOwn({}, ""), false);
+    assert.equal(utils.hasOwn({ a: 1 }, "a"), true);
+    assert.equal(utils.hasOwn({ a: {} }, "a"), true);
+    assert.equal(utils.hasOwn({ a: [] }, "a"), true);
+    assert.equal(utils.hasOwn({}, "a"), false);
+    assert.equal(utils.hasOwn([], 0), false);
+    assert.equal(utils.hasOwn([1], 0), true);
+    assert.equal(utils.hasOwn([1], "0"), true);
+    assert.equal(utils.hasOwn([], "0"), false);
+
+    assert.equal(utils.hasOwn(Object.create(null), ""), false);
+    assert.equal(utils.hasOwn(Object.create(null), "a"), false);
+    assert.equal(utils.hasOwn({}), false);
+    assert.equal(utils.hasOwn([]), false);
+    assert.equal(utils.hasOwn(Object.create(null)), false);
+  });
+  it("entries", function () {
+    assert.deepEqual(utils.entries({ a: 1 }), [["a", 1]]);
+    assert.deepEqual(utils.entries([1]), [["0", 1]]);
+    assert.deepEqual(utils.entries({ a: 1, b: { v: 1 } }), [
+      ["a", 1],
+      ["b", { v: 1 }],
+    ]);
+    assert.deepEqual(utils.entries(Object.create(null)), []);
+    assert.deepEqual(utils.entries({}), []);
+    assert.deepEqual(utils.entries([]), []);
+  });
   it("checkObject", function () {
     assert.equal(utils.checkObject({}), undefined);
     assert.equal(utils.checkObject([]), undefined);
@@ -93,3 +160,12 @@ describe("utils", () => {
     });
   });
 });
+
+function fresh(file) {
+  file = require.resolve(file);
+  var tmp = require.cache[file];
+  delete require.cache[file];
+  var mod = require(file);
+  require.cache[file] = tmp;
+  return mod;
+}
